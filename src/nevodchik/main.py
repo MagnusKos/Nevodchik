a0c61c893b7af21791bb6848de8baaf9ee49de79
MagnusKos <magnuskos@gmail.com>
2026-01-02 02:22:16 +0300

    Delete conditional for launch as a script
    We're using pyproject.toml and containers, so name==main is redundant
    and may cause problems.

--- src/nevodchik/main.py
+++ src/nevodchik/main.py
@@ -5,9 +5,8 @@ mqtt_port = os.environ.get("MQTT_PORT")
 mqtt_user = os.environ.get("MQTT_USER")
 mqtt_pass = os.environ.get("MQTT_PASS")
 
+
 def run():
     print("Fisherman is catching fish...")
     print("Host={}:{}, User={}:{}".format(mqtt_host, mqtt_port, mqtt_user, mqtt_pass))
-
-if __name__ == "__main__":
-    run()
+    pass
