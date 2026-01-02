ea6496fe2a01ab70924fe5e6f50c079c645b804d
MagnusKos <magnuskos@gmail.com>
2026-01-02 18:11:16 +0300

    Main update
    We don't need env-vars here anymore.
    Now "run" method more specific: it looks like an actual entry-point that
    connects parts of the application.

--- src/nevodchik/main.py
+++ src/nevodchik/main.py
@@ -1,12 +1,22 @@
-import os
+import logging
 
-mqtt_host = os.environ.get("MQTT_HOST")
-mqtt_port = os.environ.get("MQTT_PORT")
-mqtt_user = os.environ.get("MQTT_USER")
-mqtt_pass = os.environ.get("MQTT_PASS")
+from .config import ConfigApp
+from .client_mqtt import ClientMQTT
+from .message_processor import MessageProcessor
 
+logger = logging.getLogger(__name__)
 
 def run():
     print("Fisherman is catching fish...")
-    print("Host={}:{}, User={}:{}".format(mqtt_host, mqtt_port, mqtt_user, mqtt_pass))
+    config = ConfigApp("./tests/test.conf.local")
+    print(f"{str(config)}")
+    processor = MessageProcessor(config)
+
+    client_mqtt = ClientMQTT(config, processor)
+
+    try:
+        client_mqtt.run()
+    except KeyboardInterrupt:
+        logger.warning("Keyboard interruption, exiting...")
+        print("Keyboard interruption, exiting...")
     pass
