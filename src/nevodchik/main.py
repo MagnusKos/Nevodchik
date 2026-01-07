3c004bc472bfbdea962cb83159e59a311127246b
MagnusKos <magnuskos@gmail.com>
2026-01-07 03:25:09 +0300

    Config argument
    Added config-set logic to the main app:
    Path to the config-file could be set either via env CONFIG_FILE or via
    -c argument. If neither set, then the default value is in use.

--- src/nevodchik/main.py
+++ src/nevodchik/main.py
@@ -1,17 +1,36 @@
+import os
 import logging
+import argparse
 
 from .config import ConfigApp
 from .client_mqtt import ClientMQTT
 from .message_processor import MessageProcessor
 
+default_config_file = "./config/nevodchik.conf"
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "CRITICAL")
+logging.basicConfig(
+    level=getattr(logging, LOG_LEVEL),
+    datefmt="%H:%M:%S",
+    format="%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s",
+)
 logger = logging.getLogger(__name__)
 
-def run():
-    print("Fisherman is catching fish...")
-    config = ConfigApp("./tests/test.conf.local")
-    print(f"{str(config)}")
-    processor = MessageProcessor(config)
+parser = argparse.ArgumentParser(prog="Nevodchik")
+parser.add_argument("-c", "--config", type=str)
 
+
+def run():
+    args = parser.parse_args()
+    config = ConfigApp(
+        args.config or os.environ.get("CONFIG_FILE") or default_config_file
+    )
+
+    print("Fisherman is catching fish...")
+
+    logger.info(f"{str(config)}")
+
+    processor = MessageProcessor(config)
     client_mqtt = ClientMQTT(config, processor)
 
     try:
